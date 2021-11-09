d0cc5513936c248fb4298aa5c1f15938ff070cf5
kinzer <kinzerai@yahoo.com>
2021-11-09 18:23:22 +0300

    Prog002 added

---
+++ Prog002/Program.cs
@@ -0,0 +1,13 @@
+﻿// сумма 2-х чисел
+
+Console.WriteLine("Считаем сумму");
+int numberA = 9;
+int numberB = 5;
+int result = numberA + numberB; 
+Console.WriteLine(result);
+
+//Console.WriteLine("Считаем сумму"); - др версия
+//int numberA = 3;
+//int numberB = 5;
+//Console.WriteLine(numberA + numberB);
+
