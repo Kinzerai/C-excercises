fa782bec45fef8d86570b07c6ee19b8c3cc2b861
kinzer <kinzerai@yahoo.com>
2021-11-09 18:27:49 +0300

    AVG AMT

---
+++ Prog003/Program.cs
@@ -0,0 +1,13 @@
+﻿// Среднее арифметическое
+
+double a,b,c,d,S;
+Console.Write("a:");
+a=Convert.ToInt32(Console.ReadLine());
+Console.Write("b:");
+b=Convert.ToInt32(Console.ReadLine());
+Console.Write("c:");
+c=Convert.ToInt32(Console.ReadLine());
+Console.Write("d:");
+d=Convert.ToInt32(Console.ReadLine());
+S=(a+b+c+d)/4;
+Console.WriteLine(S);
