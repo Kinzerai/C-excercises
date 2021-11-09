1fda91e773a1b13070f945da57005aad6efb003d
kinzer <kinzerai@yahoo.com>
2021-11-09 21:28:58 +0300

    NewRandom ex added

---
+++ Prog007/Program.cs
@@ -0,0 +1,9 @@
+﻿
+// сумма случайных чисел
+
+int numberA = new Random().Next(1, 10); // те будет распространяться на значения 1,2,3,4,5,6,7,8,9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB; 
+Console.WriteLine(result);
