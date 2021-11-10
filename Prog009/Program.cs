a75b537f7f1ae86da594500f579e8c874b0b4169
kinzer <kinzerai@yahoo.com>
2021-11-10 13:30:09 +0300

    Ex on finding max of 5

---
+++ Prog009/Program.cs
@@ -0,0 +1,18 @@
+﻿//задача с гирями: найти самую тяжелую из 5
+
+int a = 4;
+int b = 10;
+int c = 16;
+int d = 2;
+int e = 25;
+ 
+ int max = a;
+
+ if (a > max) max = a;
+ if (b > max) max = b;
+ if (c > max) max = c;
+ if (d > max) max = d;
+ if (e > max) max = e;
+
+ Console.Write("max = ");
+ Console.WriteLine(max);
