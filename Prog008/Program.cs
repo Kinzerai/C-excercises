a002dc9da96743098cb51b26f19aa41cd008933d
kinzer <kinzerai@yahoo.com>
2021-11-10 13:16:45 +0300

    Ex on greeting different users

---
+++ Prog008/Program.cs
@@ -0,0 +1,22 @@
+﻿//Приветствие разных пользователей
+
+Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "лика")
+{
+    Console.WriteLine("Ура, это же сама Лика!!!");
+}
+
+if (username.ToLower() == "илья")
+{
+    Console.WriteLine("Неужели это сам Илья!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+    Console.WriteLine("!");
+
+
+}
