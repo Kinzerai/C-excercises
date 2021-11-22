14e52c4c8730435017c450e776589e074796ce8a
kinzer <kinzerai@yahoo.com>
2021-11-22 11:01:29 +0300

    Является ли число квадратом другого

---
+++ Prog012/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int result1 = numberA*numberA;
+
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result2 = numberB*numberB;
+
+if (result1 == result2) 
+
+{
+Console.WriteLine ("Верно");
+}
+else
+{
+    Console.WriteLine("Не квадрат!");
+}
