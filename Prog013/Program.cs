0d210ce232f6a9e295a9be1d9e9a1772ce632903
kinzer <kinzerai@yahoo.com>
2021-11-22 12:43:07 +0300

    Даны два числа. Показать большее и меньшее число

---
+++ Prog013/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+int numberA = new Random().Next(1, 10); 
+int numberB = new Random().Next(1, 10);
+
+if (numberA < numberB)
+
+{
+    Console.Write(numberA);
+    Console.WriteLine(" - min");
+    Console.Write(numberB);
+    Console.WriteLine(" - max");
+}
+
+else if (numberA == numberB)
+    {
+      Console.WriteLine(numberA);
+      Console.WriteLine(numberB);  
+      Console.WriteLine("Значения равны");
+    }   
+
+
+else 
+{
+Console.Write(numberA);
+Console.WriteLine(" - max");
+Console.Write(numberB);
+Console.WriteLine(" - min");
+}
+
+
