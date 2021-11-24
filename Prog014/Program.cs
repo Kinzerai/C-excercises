d824f521948f4fe57f5583238a001a03deb68144
kinzer <kinzerai@yahoo.com>
2021-11-24 11:56:18 +0300

    кратно ли число заданному, если нет, вывести остаток

---
+++ Prog014/Program.cs
@@ -0,0 +1,8 @@
+﻿// Math.DivRem 
+// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+int numA = 3;
+int numB = 16;
+double result = (numB/numA);
+Console.WriteLine(result);
+Console.WriteLine(numB % numA);
