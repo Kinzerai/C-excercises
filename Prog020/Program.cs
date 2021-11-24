12e281864d0df99b0e8b6f0f24d82f7253244395
kinzer <kinzerai@yahoo.com>
2021-11-24 20:46:56 +0300

    Найти максимальное из трех чисел

---
+++ Prog020/Program.cs
@@ -0,0 +1,20 @@
+﻿// Найти максимальное из трех чисел
+
+int NumA = 13;
+int NumB = 6;
+int NumC = 9;
+
+int max = NumA;
+
+if (NumB > max) 
+{
+    max = NumB;
+}
+
+if (NumC > max) 
+{
+    max = NumC;
+}
+
+
+Console.WriteLine(max);
