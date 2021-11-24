4d5b37cfe3d08baceb79662e680b82c1f358ffdd
kinzer <kinzerai@yahoo.com>
2021-11-24 12:20:17 +0300

    Выяснить является ли число чётным

---
+++ Prog015/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить является ли число чётным
+
+int numberA = 20;
+
+if (numberA % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else 
+{
+    Console.WriteLine("Число нечетное");
+}
