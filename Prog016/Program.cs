a833487117fe16a5ee22a1e80d889e920c3213e6
kinzer <kinzerai@yahoo.com>
2021-11-24 16:35:28 +0300

    Найти индекс в массиве по заданному числу

---
+++ Prog016/Program.cs
@@ -0,0 +1,17 @@
+﻿// Intro_Array первая задача с массивами с лекции 2
+
+int[] array = { 1, 12, 13, 43, 2, 76, 45, 34, 21, 34, 7, 9};
+
+int n = array.Length;
+int find = 34;
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
