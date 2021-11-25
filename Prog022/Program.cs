e2161fec43af657f831de205e6f770a86d66a6e7
kinzer <kinzerai@yahoo.com>
2021-11-25 12:47:45 +0300

    функция y=f(a)

---
+++ Prog022/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+//f(x)=x2 +1
+//double f(double x)
+//{
+  //  double result = x*x +1;
+    //return result
+//}
+
+
+// функция y=f(a)
+
+Console.Clear();
+
+int number = new Random().Next(1, 20);
+double f(double a)
+{
+    return (a*a+1);
+}
+Console.WriteLine(number);
+Console.WriteLine(f(number));
+
