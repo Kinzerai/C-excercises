fe7ce42016336559c7b7ebb7507a413e57798b3b
kinzer <kinzerai@yahoo.com>
2021-11-25 12:47:08 +0300

    По заданному номеру дня недели вывести его название

---
+++ Prog021/Program.cs
@@ -0,0 +1,14 @@
+﻿//По заданному номеру дня недели вывести его название
+
+Console.Clear();
+string[] Weekdays = {" ","Mon", "Tue", "Wed", "Thr", "Fr", "Sat", "Sun"};
+
+Console.Write("Enter number: ");
+
+int index = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Weekdays[index]);  
+
+
+
+
