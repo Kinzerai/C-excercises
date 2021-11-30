3527b8eb2c3de6abd02b2a87d8af4025f59a7a0d
kinzer <kinzerai@yahoo.com>
2021-12-01 02:36:45 +0300

    Показать последнюю цифру трёхзначного числа

---
+++ Task09/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int A = int.Parse(Console.ReadLine());
+int B = (A%100);
+int C = (B%10);
+
+Console.WriteLine(C);
