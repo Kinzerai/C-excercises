2bbb932648b2bc4bf46aad7a77afe650b17f13cb
kinzer <kinzerai@yahoo.com>
2021-11-30 19:17:12 +0300

    Показать числа от -N до N

---
+++ Task07/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать числа от -N до N
+
+Console.Clear();
+Console.WriteLine("Введите любое число: ");
+int NumberN = Convert.ToInt32(Console.ReadLine());
+int NumberMinusN = - NumberN;
+int count = -NumberN;
+int result = - NumberN-1;
+
+while (count <= NumberN)
+{
+    result = result + 1; 
+Console.WriteLine(result);
+count++;
+}
+
+
+
