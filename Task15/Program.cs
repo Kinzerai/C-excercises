71bd2d1e2ab61840e4e2ac591fdce4e1eca7efa4
kinzer <kinzerai@yahoo.com>
2021-12-01 02:56:14 +0300

    Дано число. Проверить кратно ли оно 7 и 23

---
+++ Task15/Program.cs
@@ -0,0 +1,8 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+Console.Clear();
+int NumA = 49;
+int NumB = 7;
+int NumC = 23;
+
+if ((NumA % NumB == 0) && (NumA % NumC ==0)) Console.WriteLine("Число"+" " + NumA + " "+ "кратно 7 и 23");
+else Console.WriteLine("Число" + " " + NumA + " " + "не делится без остатка на 7 и 23");
