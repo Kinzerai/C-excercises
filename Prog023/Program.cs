8640d25253fef98652e936641f4a6b68ef52ffa9
kinzer <kinzerai@yahoo.com>
2021-11-30 18:50:55 +0300

    Показать четные числа от 1 до N

---
+++ Prog023/Program.cs
@@ -0,0 +1,23 @@
+﻿
+// Показать четные числа от 1 до N
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int numberB = 1;
+int result = 0;
+int count = 1;
+
+while (count < numberN)
+{
+    result = numberB + count;
+    if (result % 2 == 0)
+    {
+        Console.WriteLine(result);
+    }
+    count++; 
+}
+
+
+
+
