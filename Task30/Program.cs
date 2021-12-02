1523815c1113bab747e1b408533968337264a944
kinzer <kinzerai@yahoo.com>
2021-12-03 01:23:35 +0300

    Показать кубы чисел, заканчивающихся на четную цифру

---
+++ Task30/Program.cs
@@ -0,0 +1,11 @@
+﻿// 30. Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.Clear();
+Console.Write("Введите четное число: ");
+int NumA = Convert.ToInt32(Console.ReadLine());
+
+if (NumA % 2 != 0) Console.WriteLine("Вы ввели нечетное число!");
+else Console.WriteLine($"Куб числа {NumA} равен {Math.Pow(NumA,3)}");
+
+
+
