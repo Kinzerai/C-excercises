22e00b8aa768a4ac58b5c0bb8350ff3340f36a3b
kinzer <kinzerai@yahoo.com>
2021-12-03 01:25:27 +0300

    Найти кубы чисел от 1 до N

---
+++ Task24/Program.cs
@@ -0,0 +1,16 @@
+﻿//Найти кубы чисел от 1 до N
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+double NumA = Convert.ToInt32(Console.ReadLine());
+double count = 0;
+double result = 0;
+double cubic = 0;
+
+while (count < NumA)
+{
+    result = result + 1;
+    cubic = Math.Pow(result, 3);
+    Console.WriteLine($"Куб числа {result} равен {cubic} ");
+    count++;
+}
