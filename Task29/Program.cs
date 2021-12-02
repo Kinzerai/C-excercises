caf8c40b0c1081b74648616a2db5c4d811fbf3d1
kinzer <kinzerai@yahoo.com>
2021-12-03 01:24:13 +0300

    Написать программу вычисления произведения чисел от 1 до N

---
+++ Task29/Program.cs
@@ -0,0 +1,18 @@
+﻿// 29. Написать программу вычисления произведения чисел от 1 до N
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+double NumA = Convert.ToInt32(Console.ReadLine());
+double count = 0;
+double result = 0;
+double multi = 1;
+
+while (count < NumA)
+{
+    result = result + 1;
+    multi = multi*result;
+    count++;
+
+}
+
+Console.WriteLine($"Произведение чисел от 1 до {NumA} равно {multi} ");
