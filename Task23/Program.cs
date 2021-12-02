cc11642ed944b9627ecc8b728c9ff699ae810eae
kinzer <kinzerai@yahoo.com>
2021-12-03 01:25:03 +0300

    Показать таблицу квадратов чисел от 1 до N

---
+++ Task23/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+Console.Clear();
+Console.WriteLine("Введите число: ");
+double NumA = Convert.ToInt32(Console.ReadLine());
+double count = 0;
+double result = 0;
+double sqr = 0;
+
+while (count < NumA)
+{
+    result = result + 1;
+    sqr = Math.Pow(result,2);
+
+    Console.WriteLine($"Квадрат числа {result} равен: {sqr} ");
+
+    count++;
+}
