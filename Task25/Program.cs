20998087b052ae4435ca3d2697e2b1966e1ac5d9
kinzer <kinzerai@yahoo.com>
2021-12-03 00:31:10 +0300

    Найти сумму чисел от 1 до А

---
+++ Task25/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти сумму чисел от 1 до А
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int NumA = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+int count = 0;
+int sum = 0;
+
+while (count < NumA)
+{
+    result = result + 1;
+    sum = sum + result;
+    count++;
+}
+
+Console.WriteLine(sum);
