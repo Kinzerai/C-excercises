5c0da857787b73952c88a4384a829ef81d8016fb
kinzer <kinzerai@yahoo.com>
2021-12-03 00:07:39 +0300

    Найти третью цифру числа или сообщить, что её нет

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -1,9 +1,37 @@
 ﻿// Найти третью цифру числа или сообщить, что её нет
 Console.Clear();
-Console.WriteLine("Введите любое число: ");
-string NumberA = Convert.ToString(Console.ReadLine());
-Console.WriteLine(NumberA[2]);
+
+void Find3Num(int NumberA)
+
+{
+    string NumberB = NumberA.ToString();
+
+    if (NumberB.Length > 2) Console.WriteLine($"Третья цифра числа: {NumberB[2]}");
+    else Console.WriteLine("Нет третьей цифры");
+}
+
+Console.Write("Введите любое число: ");
+
+Find3Num(Convert.ToInt32(Console.ReadLine()));
+
+//
+//Console.Clear();
+
+//void Find3Num(string NumberA)
+
+//{if (NumberA.Length > 2) Console.WriteLine($"Третья цифра числа: {NumberA[2]}");
+//else Console.WriteLine("Нет третьей цифры");}
+
+//Console.Write("Введите любое число: ");
+//string NumberA = Console.ReadLine();
+
+Find3Num(Console.ReadLine());
+
+
+
+
+
 
 //if(NumberA[2]== 0) Console.WriteLine("В числе меньше трех цифр");
- 
+
 //string.Empty - пока не знаю как вывести что нет цифры
