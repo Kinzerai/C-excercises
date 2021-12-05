25d692e67403610916c68cf95ba46df3c03fe16c
kinzer <kinzerai@yahoo.com>
2021-12-06 00:58:44 +0300

    Определить количество цифр в числе

---
+++ Task27/Program.cs
@@ -0,0 +1,7 @@
+﻿// 27. Определить количество цифр в числе
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+string Num = Console.ReadLine();
+int length = Num.Length;
+Console.WriteLine(length);
