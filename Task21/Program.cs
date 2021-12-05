ca0b484735015129932cbd8102a628e0d87eb8e7
kinzer <kinzerai@yahoo.com>
2021-12-05 23:48:34 +0300

    Программа проверяет пятизначное число на палиндромом

---
+++ Task21/Program.cs
@@ -0,0 +1,13 @@
+﻿// 21. Программа проверяет пятизначное число на палиндромом.
+
+Console.Clear();
+Console.WriteLine("Введите числовой палиндром из 5 чисел: ");
+string Num = Console.ReadLine();
+int length = Num.Length;
+
+if (length > 5 || length < 5) Console.WriteLine("Неверно введено число!");
+if (Num[0] == Num[4] && Num[1] == Num[3]) Console.WriteLine($"Число {Num} - палиндром");
+else Console.WriteLine($"Число {Num} не является палиндромом");
+
+
+
