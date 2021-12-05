5a9072fc94d170d9a3962a6fb4eae31b9e38217c
kinzer <kinzerai@yahoo.com>
2021-12-06 00:58:18 +0300

    Возведите число А в натуральную степень B используя цикл

---
+++ Task26/Program.cs
@@ -0,0 +1,14 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+
+Console.Clear();
+int numA = new Random().Next(-10,10);
+int numB = new Random().Next(1,3);
+Console.WriteLine(numA);
+Console.WriteLine(numB);
+int result = 1;
+for (int count = 0; count<numB; count++)
+{
+result = result * numA;
+}
+Console.WriteLine(result);
