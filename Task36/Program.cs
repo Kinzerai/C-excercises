f4e4eedddb5518b69087d452ac87f68ae4ba299e
kinzer <kinzerai@yahoo.com>
2021-12-06 17:07:51 +0300

    Массив трехзначные числа, кол-во четных и нечетных

---
+++ Task36/Program.cs
@@ -0,0 +1,31 @@
+﻿// 36. Задать массив, заполнить случайными положительными трёхзначными числами. 
+//Показать количество нечетных\четных чисел
+Console.Clear();
+
+int[] array = new int[7];
+
+Console.Write("Массив {");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    Console.Write(" " + array[i] + " ");
+}
+Console.WriteLine("}");
+
+int count1 =0;
+int count2 =0;
+
+for (int j = 0; j<array.Length; j++)
+{
+    if (array[j] % 2 ==0) 
+    {
+        count1 = count1 + 1;
+    }
+    if (array[j] % 2 !=0)
+    {
+        count2 = count2+1;
+    }
+}
+Console.WriteLine($"В массиве {count1} четных и {count2} нечетных чисел");
+
+
