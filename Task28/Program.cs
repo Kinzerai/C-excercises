6d06c2c432ed90298aa1716ac5f909758f243587
kinzer <kinzerai@yahoo.com>
2021-12-06 13:52:27 +0300

    Подсчитать сумму цифр в числе

---
+++ Task28/Program.cs
@@ -0,0 +1,15 @@
+﻿//28.  Подсчитать сумму цифр в числе
+Console.Clear();
+Console.WriteLine("Введите число: ");
+string Num = Console.ReadLine();
+int length = Num.Length;
+int NumB = 0;
+int sum = 0;
+
+for (int index = 0; index < length; index++)
+{
+    NumB = Convert.ToInt32(Num[index] - '0');
+    Console.WriteLine($"Цифра: {NumB}");
+    sum = sum + NumB;
+}
+Console.WriteLine($"Сумма всех цифр числа {Num} равна {sum}.");
