e0d08d25a3db43079119adb2575c0a442eeac44e
kinzer <kinzerai@yahoo.com>
2021-12-06 17:04:10 +0300

    Задать массив из 8 элементов и вывести их на экран

---
+++ Task31/Program.cs
@@ -0,0 +1,17 @@
+﻿//31. Задать массив из 8 элементов и вывести их на экран
+
+Console.Clear();
+
+int[] array = new int[8];
+//Random rnd = new Random();
+
+Console.Write("{");
+for (int i=0; i<array.Length; i++)
+{
+    array[i] = new Random().Next(0,100);
+    Console.Write(" " + array[i]+ " ");
+}
+Console.WriteLine("}");
+
+
+
