2a5c05d32baff4ee29d83db3c6ff45dcb26c0368
kinzer <kinzerai@yahoo.com>
2021-12-06 17:06:13 +0300

    замену элементов массива на противоположные

---
+++ Task34/Program.cs
@@ -0,0 +1,23 @@
+﻿//34. Написать программу замену элементов массива на противоположные
+
+Console.Clear();
+
+int[] array = new int[10];
+
+Console.Write("Массив {");
+for (int i=0; i<array.Length; i++)
+{
+    array[i] = new Random().Next(-10,11);
+    Console.Write(" " + array[i]+ " ");
+}
+Console.WriteLine("}");
+
+
+Console.Write("Новый массив {");
+
+for (int j=0; j<array.Length; j++) 
+{
+    array[j]= - array[j];
+    Console.Write(" " + array[j]+ " ");
+}
+Console.WriteLine("}");
