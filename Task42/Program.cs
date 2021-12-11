389b2c4f09bd73ac48877042987e37b27170f5a2
kinzer <kinzerai@yahoo.com>
2021-12-12 00:54:06 +0300

    Определить сколько чисел больше 0 введено с клавиатуры

---
+++ Task42/Program.cs
@@ -0,0 +1,21 @@
+﻿// 42. Определить сколько чисел больше 0 введено с клавиатуры
+
+Console.Clear();
+int[] array = new int[6];
+Console.WriteLine("Введите 6 элементов массива: ");
+
+for (int i =0; i<6; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int count = 0;
+for (int j=0; j<6; j++)
+{
+   if (array[j]>0) count = count +1; 
+}
+
+Console.WriteLine($"Больше 0 введено {count} ");
+
+
+
